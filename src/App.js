ea655532fe510680b684c08711201ecce3904268
Hadia <hadiaaanvd@gmail.com>
2021-07-27 01:51:38 +0500

    Header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Switch>
         <Route exact path="/" component={Homepage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import "./homepage.style.scss";
 import Directory from '../../components/directory/directory.component';
 
+
 const Homepage = () => {
 return(
     <div className='homepage'>
